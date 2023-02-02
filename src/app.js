c714e2fbc51c6810daf13b179ada10267ef0bff9
BontleMokose <mokosebontle@gmail.com>
2023-02-01 22:56:20 -0800

    display forecast using js and and index

--- src/app.js
+++ src/app.js
@@ -23,6 +23,41 @@ return `${day} ${hours}:${minutes}`;
 
 }
 
+function displayForecast() {
+  let forecastElement =document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days =["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+ forecastHTML = 
+  forecastHTML +
+  `
+                <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+                    
+                    <img
+                    src="https://openweathermap.org/img/wn/50d@2x.png"
+                    alt=""
+                    width="42"
+                    />
+                    <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">
+                        18°
+                    </span>
+                    <span class="weather-forecast-temperature-min">
+                    12°
+                    </span>
+                </div>
+                </div>
+                
+                `;
+  })
+ 
+                forecastHTML = forecastHTML+ `</div>`;
+                forecastElement.innerHTML = forecastHTML;
+}
+  
+
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector
@@ -38,6 +73,9 @@ function displayTemperature(response) {
   let dateElement = document.querySelector 
   ("#date");
   let iconElement = document.querySelector("#icon");
+
+  
+
 
   celsiusTemperature = response.data.main.temp;
 
@@ -87,6 +125,7 @@ function displayCelsiusTemperature(event) {
   let celsiusTemperature = null;
 
 
+
   let form= document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
 
@@ -97,4 +136,6 @@ farenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-  search("New York")
+
+displayForecast();
+search("New York");
