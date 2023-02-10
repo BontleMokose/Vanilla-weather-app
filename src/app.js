5b652127c4ce1fa0e9b7d62d63c805513417e403
BontleMokose <mokosebontle@gmail.com>
2023-02-09 20:22:04 -0800

    Getting API call for forecast

--- src/app.js
+++ src/app.js
@@ -23,11 +23,13 @@ return `${day} ${hours}:${minutes}`;
 
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement =document.querySelector("#forecast");
+ 
+  let days =["Thu", "Fri", "Sat", "Sun"];
 
   let forecastHTML = `<div class="row">`;
-  let days =["Thu", "Fri", "Sat", "Sun"];
   days.forEach(function(day) {
  forecastHTML = 
   forecastHTML +
@@ -57,7 +59,12 @@ function displayForecast() {
                 forecastElement.innerHTML = forecastHTML;
 }
   
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector
@@ -88,6 +95,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
   
+ getForecast(response.data.coord);
+
 }
 
 function search(city) {
@@ -137,5 +146,5 @@ farenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-displayForecast();
+
 search("New York");
